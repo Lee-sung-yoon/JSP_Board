1723292c7eedab3be66056f71665377340dc0b95
Lee-sung-yoon <tjddbs1412@naver.com>
2023-12-11 15:26:37 +0900

    구구단 서블릿에서 계산된 것을 jsp으로 보내서 출력하기

--- src/main/java/sbs/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/sbs/jsp/board/servlet/GugudanServlet.java
@@ -1,5 +1,6 @@
 package sbs.jsp.board.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,13 +19,18 @@ public class GugudanServlet extends HttpServlet {
         int dan = rq.getIntParam("dan", 9);
         int limit = rq.getIntParam("limit", 9);
 
-        rq.appendBody("<h1>%d</h1>\n".formatted(dan));
+        // request 정보를 담는다.
+        // 왜냐하면 나중에 gugudan2.jsp에서 해당 내용을 꺼내 쓰기 위해서이다.
+//        rq.setAttr("dan", dan);
+//        rq.setAttr("limit", limit);
 
-        for (int i = 1; i <= limit; i++) {
-            rq.appendBody("<div> %d * %d = %d </div>\n".formatted(dan, i, dan * i));
-        }
+        req.setAttribute("dan", dan);
+        req.setAttribute("limit", limit);
 
-        rq.appendBody("<div class=\"a\"></div>");
-        rq.appendBody("<style>.a {width: 200px; height: 200px; background-color: red;}</style>");
+        // gugudan2.jsp 에게 나머지 작업을 토스
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/gugudan2.jsp");
+        requestDispatcher.forward(req, resp);
+
     }
 }
+
