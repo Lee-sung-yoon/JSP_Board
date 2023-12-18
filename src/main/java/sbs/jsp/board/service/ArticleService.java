232c5e2ca42e7e666f88c6152bb3ade981b7392d
Lee-sung-yoon <tjddbs1412@naver.com>
2023-12-18 23:13:32 +0900

    detail을 서비스와 리포지터리로 분리하기

--- src/main/java/sbs/jsp/board/service/ArticleService.java
+++ src/main/java/sbs/jsp/board/service/ArticleService.java
@@ -1,5 +1,6 @@
 package sbs.jsp.board.service;
 
+import sbs.jsp.board.dto.Article;
 import sbs.jsp.board.dto.ResultData;
 import sbs.jsp.board.repository.ArticleRepository;
 import sbs.jsp.board.util.MysqlUtil;
@@ -40,5 +41,9 @@ public class ArticleService {
     public ResultData write(int loginedMemberId, String title, String content) {
         int id = articleRepository.write(loginedMemberId, title, content);
         return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), "id", id);
+    }
+
+    public Article getForPrintArticleById(int id) {
+        return articleRepository.getForPrintArticleById(id);
     }
 }
